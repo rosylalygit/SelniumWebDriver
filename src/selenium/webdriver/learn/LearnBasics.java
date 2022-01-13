7f22a597a8a534795439177d4ef38eb1e3566fd8
jijop <jijop@DESKTOP-SLA9VAV.attlocal.net>
2022-01-12 22:25:56 -0500

    Very First commit

---
+++ src/selenium/webdriver/learn/LearnBasics.java
@@ -0,0 +1,34 @@
+package selenium.webdriver.learn;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnBasics {
+
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriverwin32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
+	
+		
+		driver.findElement(By.linkText("Sign in")).click();
+		//driver.findElement(By.id("email")).sendKeys("rosy.laly@gmail.com");
+		driver.findElement(By.cssSelector("input#email")).sendKeys("rosy.laly");
+		driver.findElement(By.id("passwd")).sendKeys("1234");
+		String exp_title="Login - My Store";
+		String act_title=driver.getTitle();
+		if (exp_title.equals(act_title)) {
+			System.out.println("Test Pass");
+		}
+		else
+			System.out.println("Tetst failed");
+		
+//		driver.close();
+
+	}
+
+}
---
+++ src/selenium/webdriver/learn/LearnXpath.java
@@ -0,0 +1,28 @@
+package selenium.webdriver.learn;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnXpath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//*[@id="email"]
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriverwin32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://facebook.com");
+		driver.manage().window().maximize();
+	
+		
+		
+		driver.findElement(By.xpath("//*[@id='email']"));
+		
+//		driver.close();
+		
+		//*[@id="email"]
+		
+	}
+
+}
